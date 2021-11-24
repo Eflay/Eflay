6915ff02248d452504de4ff82d3cb4551c4303f6
Eflay <71784412+Eflay@users.noreply.github.com>
2021-11-24 19:41:37 +0100

    Delete Progra directory

--- Progra/Cours-FullStack/BackEnd/models/Things.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const thingSchema = mongoose.Schema({
-  imageURL: { type: String, require: true },
-});
-
-module.exports = mongoose.model("Things", thingSchema);
--- Progra/Cours-FullStack/BackEnd/routes/User.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const auth = require("../controllers/Auth");
-
-router.use(express.json());
-router.post("/signup", auth.signup);
-router.post("/signin", auth.signin);
-
-module.exports = router;
